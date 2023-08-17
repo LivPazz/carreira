091e0ca482f712938e7529b8d612bf5466b4095f
livdassa <livia.paz@etec.sp.gov.br>
2023-08-17 11:36:43 -0300

    Arquivos adicionados

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible">
+        <meta name="viewport" content="w">
+        <title>Index</title>
+</head>
+<body>
+    <h1>Index</h1>
+</body>
+</html>
+
+    
